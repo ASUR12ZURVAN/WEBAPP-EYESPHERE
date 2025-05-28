51c51fc109488b1183de2b7415922f29301106df
ASUR12ZURVAN <143814906+ASUR12ZURVAN@users.noreply.github.com>
2025-05-28 19:46:31 +0530

    More changes

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -98,9 +98,18 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
 
 
 # Password validation
