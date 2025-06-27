d5e5b4157400379cfc7b6c3a85248e3beff37f6b
ASUR12ZURVAN <143814906+ASUR12ZURVAN@users.noreply.github.com>
2025-06-27 16:52:39 +0530

    Database changes for deployment

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
@@ -112,7 +112,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 
 # Password validation
